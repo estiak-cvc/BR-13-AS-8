de24868f0d07c4dc97cd007b5ee1a8a24fe3aa51
estiak-cvc <confidenc.esth@gmail.com>
2026-05-06 08:27:31 +0600

    import dns done

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,9 +1,12 @@
+import dns from "node:dns";
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
-const db = client.db('skillsph');
+const db = client.db();
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
